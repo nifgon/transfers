d232574d1080d29477262c761715659e74da1289
nifgon <nifgon@gmail.com>
2026-06-01 18:10:40 +0300

    fix: correct paths for GitHub Pages subdirectory

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'transfers-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = ['/transfers/', '/transfers/index.html', '/transfers/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
